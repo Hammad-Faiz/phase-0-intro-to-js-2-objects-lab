ea2938b9d8ae613fabdecbc0668a9c5fe1fc809b
Hammad <hammadfaiz172@gmail.com>
2022-05-28 14:11:10 -0400

    commit tree 75a238716b58836612a46bf841f809527479f7d7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+
+const employee = {name: 'Sam', streetAddress: '11 Broadway'}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   return employee;
+}
+
+const obj = [...employee];
+delete obj.sam;
